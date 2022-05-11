3ac1d7fd02475978e61536aed8960aca5ccd2746
Samuel <91400290+sam-kmn@users.noreply.github.com>
2022-05-11 02:20:52 +0200

    fix Setup && Screenshots in README

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ https://observe-me.netlify.app/
 * React
 * Tailwindcss
 * Chart.js
+
+## First look
+<img width="1552" alt="Screenshot 2022-05-11 at 02 01 34" src="https://user-images.githubusercontent.com/91400290/167744454-887e3546-ef7a-4a13-9f4b-e50afe908c38.png">
+
+## Charts
+<img width="1552" alt="Screenshot 2022-05-11 at 02 13 23" src="https://user-images.githubusercontent.com/91400290/167744516-5fdcbef8-d5de-45f8-9afd-8a896dbae5a1.png">
+
+## Table
+<img width="1552" alt="Screenshot 2022-05-11 at 02 13 45" src="https://user-images.githubusercontent.com/91400290/167744534-3336e470-8e9e-4e5f-b4b1-3d3483b488e1.png">
--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -3,7 +3,7 @@ import useLocalStorage from '../hooks/useLocalStorage'
 
 const Setup = () => {
   
-  const [columns, setColumns] = useLocalStorage('columns', {})
+  const columns = useLocalStorage('columns', {})
 
 
   return (
@@ -12,7 +12,7 @@ const Setup = () => {
         
         <div className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">Rapidly build charts<br/>in just few seconds</div>
         <div className="text-xl sm:text-2xl md:text-3xl ">Before we start though,<br/>let's setup your columns</div>
-        <Columns setColumns={setColumns} />
+        <Columns setColumns={columns[1]} />
       
       </div>
       
